ee844460447141e0e810724bcf1f4a7290ee6bfd
Juan Carlos Vásquez López <vl25001@esfe.agape.edu.sv>
2025-05-26 09:59:19 -0600

    Se crearon los metodos de la clase futbolista y se implemento la herencia

--- Futbolista.java
+++ Futbolista.java
@@ -1,3 +1,52 @@
-public class Futbolista {
+public class Futbolista extends Persona {
+    
+    private int dorsal;
+    private String demarcacion;
+
+    public Futbolista() {
+        super();
+    }
+
+    public Futbolista(int id, String nombre, String apellido, int edad, int dorsal, String demarcacion) {
+        super(id, nombre, apellido, edad);
+        this.dorsal = dorsal;
+        this.demarcacion = demarcacion;
+    }
+
+    public int getDorsal() {
+        return dorsal;
+    }
+
+    public void setDorsal(int dorsal) {
+        this.dorsal = dorsal;
+    }
+
+    public String getDemarcacion() {
+        return demarcacion;
+    }
+
+    public void setDemarcacion(String demarcacion) {
+        this.demarcacion = demarcacion;
+    }
+
+    public void jugarPartido() {
+        System.out.println("Jugar partido como central");
+    }
+
+    public void entrenar() {
+        System.out.println("Entrenar como futbolista");
+    }
+
+    @Override
+    public void Concentrarce() {
+        System.out.println("Concentrarse como futbolista");
+    }
+
+    @Override
+    public void Viajar() {
+        System.out.println("Viajar como futbolista");
+    }
+
+
 
 }
