8959c49baf8a890165723fb14af1e04bfead8822
Jefferson Joel Gonzalez Montes <gm25001@esfe.agape.edu.sv>
2025-05-26 10:08:25 -0600

    Se implemento herencia y polimorfismo y se creo su metodo

--- Masajista.java
+++ Masajista.java
@@ -1,3 +1,47 @@
-public class Masajista {
+public class Masajista extends Persona {
+
+    private String titulacion;
+    private int annoExperiencia;
+
+    public Masajista(){
+
+    }
+
+    public Masajista(String titulacion, int annoExperiencia) {
+        this.titulacion = titulacion;
+        this.annoExperiencia = annoExperiencia;
+    }
+
+    public String getTitulacion() {
+        return titulacion;
+    }
+
+    public void setTitulacion(String titulacion) {
+        this.titulacion = titulacion;
+    }
+
+    public int getAnnoExperiencia() {
+        return annoExperiencia;
+    }
+
+    public void setAnnoExperiencia(int annoExperiencia) {
+        this.annoExperiencia = annoExperiencia;
+    }
+    
+
+    @Override
+    public void Concentrarce() {
+        System.out.println("Masajista Concentrarse");
+    }
+
+    @Override
+    public void Viajar() {
+        System.out.println("Masajista Viajar");
+    }
+
+
+    public void DarMasajes(){
+        System.out.println("Dar masajes");
+    }
 
 }
